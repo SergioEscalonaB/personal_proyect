14ece03c174fbd3f2ae3b1f6e1959dae30b1c4c4
Sergio Escalona <sergioluis2610@hotmail.com>
2026-01-31 18:23:29 -0500

    Metodo seguro abrir bd

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
-const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
-const dbPath = path.resolve(__dirname, 'CA.db');
+const dbPath = path.resolve(__dirname, process.env.DB_PATH);
 
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        console.error('Could not connect to database', err);
-    } else {
-        console.log('Connected to database');
-    }
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+	if (err) {
+		console.error('Error al abrir CA.db:', err.message);
+	} else {
+		console.log('Conectado a CA.db');
+	}
 });
 
 module.exports = db;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,7 +10,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/data', (req, res) => {
+app.get('/cobro', (req, res) => {
     db.all('SELECT * FROM COBRO', [], (err, rows) => {
         if (err) {
             return res.status(500).json({ error: err.message });
